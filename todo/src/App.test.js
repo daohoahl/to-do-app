7d01dc60e1337547adc203d93a6916efcb3d9f46
Client <duyhung81002@gmail.com>
2026-03-31 15:09:44 +0700

    Test Error 2

--- todo/src/App.test.js
+++ todo/src/App.test.js
@@ -5,8 +5,8 @@ test('renders Todo App UI', () => {
   render(<App />);
 
   // Title is always present
-  const wrongText = screen.getByText(/this text does not exist/i);
-  expect(wrongText).toBeInTheDocument();
+  const title = screen.getByText(/my to-do list/i);
+  expect(title).toBeInTheDocument();
 
   // When there are no todos, empty message is shown
   const emptyMessage = screen.getByText(/no tasks yet\. add one above!/i);
